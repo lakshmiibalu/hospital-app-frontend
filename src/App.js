bd6775f779919c75969bc14430100520f4086029
lakshmiibalu <lakshmibalu74@gmail.com>
2024-02-08 21:48:39 +0530

    view patients created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPatients from './components/AddPatients';
+import ViewPatients from './components/ViewPatients';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<AddPatients/>}/>
+        <Route path='/view' element={<ViewPatients/>}/>
       </Routes>
       </BrowserRouter>
     </div>
